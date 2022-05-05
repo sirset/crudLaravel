1fbee84147f91b74d50cac5e6c0f7cc59e0efbe4
sirset <Andresechavarriamalagon@gmail.com>
2022-05-04 20:36:51 -0500

    primer commit proyecto

---
+++ database/migrations/2022_05_05_012231_create_productos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->string("name")->unique();
+            $table->string("referencia")->unique();
+            $table->integer("precio")->default(0);
+            $table->integer("peso")->default(0);
+            $table->string("categoria");
+            $table->integer("stock");
+            $table->timestamps();
+            $table->id();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+}
